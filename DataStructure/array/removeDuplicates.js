7f4e9846a2c68f93698e16e8371f6f8942b59d39
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-05-26 23:47:04 +0430

    Solve removeDuplicates in array challenge

---
+++ DataStructure/array/removeDuplicates.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let l = 1; 
+
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[i - 1]){
+            nums[l] = nums[i]; 
+            l++; 
+        }
+    }
+        return l; 
+};
